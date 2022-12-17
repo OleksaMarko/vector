23d4748b66b587935bc9df642a5728d0679f0977
Serge Borbit <serge.borbit@gmail.com>
2022-12-17 21:59:01 +0200

    :)

--- components/View/View.tsx
+++ components/View/View.tsx
@@ -3,21 +3,43 @@ import styles from './View.module.css'
 
 export const View: React.FC = () => {
   const [cirlces, setCircles] = useState<{x: number, y: number}[]>([])
+  const [rects, setRects] = useState<{x: number, y: number}[]>([])
+  const [tool, setTool] = useState<'circle' | 'rect'>('circle')
 
   const handleClick = (e: MouseEvent) => {
     const rect = e.currentTarget.getBoundingClientRect()
     const x = e.clientX - rect.x
     const y = e.clientY - rect.y
 
-    setCircles((circles) => {
-      return [...circles, {
-        x: x,
-        y: y,
-      }]
-    })
+    switch (tool) {
+      case 'circle':
+        setCircles((circles) => {
+          return [...circles, {
+            x: x,
+            y: y,
+          }]
+        })
+        break
+      case 'rect':
+        setRects((rects) => {
+          return [...rects, {
+            x: x,
+            y: y,
+          }]
+        })
+        break
+      // case 'line':
+      // case 'path':
+      // case 'polygon':
+      // case 'polyline':
+    }
+    
   }
 
   return (
+    <>
+    <button onClick={() => setTool('circle')}>Circle</button>
+    <button onClick={() => setTool('rect')}>Rect</button>
     <svg
       className={styles.svg}
       width={500}
@@ -26,6 +48,9 @@ export const View: React.FC = () => {
     >
       {cirlces.map(({ x, y }, i) =>
         <circle cx={x} cy={y} r={10} key={i}/>)}
+      {rects.map(({ x, y }, i) =>
+        <rect x={x} y={y} width={20} height={20} key={i}/>)}
     </svg>
+    </>
   )
 }
