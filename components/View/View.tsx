6c932f69762a269b8e3192293ce86268c93fbc88
Serge Borbit <serge.borbit@gmail.com>
2022-12-18 17:21:34 +0200

    New commit.

--- components/View/View.tsx
+++ components/View/View.tsx
@@ -1,10 +1,15 @@
-import { MouseEvent, useState } from 'react'
+import { MouseEvent, WheelEvent, useState, useRef } from 'react'
 import styles from './View.module.css'
 
+const INIT_WIDTH = 500
+const INIT_HEIGHT = 500
+
 export const View: React.FC = () => {
-  const [cirlces, setCircles] = useState<{x: number, y: number}[]>([])
-  const [rects, setRects] = useState<{x: number, y: number}[]>([])
+  const [cirlces, setCircles] = useState<{ x: number; y: number }[]>([])
+  const [rects, setRects] = useState<{ x: number; y: number }[]>([])
   const [tool, setTool] = useState<'circle' | 'rect'>('circle')
+  const deltaRef = useRef(0)
+  const elRef = useRef<SVGSVGElement>(null)
 
   const handleClick = (e: MouseEvent) => {
     const rect = e.currentTarget.getBoundingClientRect()
@@ -14,18 +19,24 @@ export const View: React.FC = () => {
     switch (tool) {
       case 'circle':
         setCircles((circles) => {
-          return [...circles, {
-            x: x,
-            y: y,
-          }]
+          return [
+            ...circles,
+            {
+              x: x,
+              y: y,
+            },
+          ]
         })
         break
       case 'rect':
         setRects((rects) => {
-          return [...rects, {
-            x: x,
-            y: y,
-          }]
+          return [
+            ...rects,
+            {
+              x: x,
+              y: y,
+            },
+          ]
         })
         break
       // case 'line':
@@ -33,24 +44,40 @@ export const View: React.FC = () => {
       // case 'polygon':
       // case 'polyline':
     }
-    
+  }
+
+  const handleWheel = (e: WheelEvent) => {
+    e.preventDefault()
+
+    deltaRef.current = Math.max(0, deltaRef.current + e.deltaY)
+    if (elRef.current) {
+      elRef.current.style.width = `${Math.max(INIT_WIDTH, INIT_WIDTH + deltaRef.current)}px`
+      elRef.current.style.height = `${Math.max(INIT_HEIGHT, INIT_HEIGHT + deltaRef.current)}px`
+    }
   }
 
   return (
     <>
-    <button onClick={() => setTool('circle')}>Circle</button>
-    <button onClick={() => setTool('rect')}>Rect</button>
-    <svg
-      className={styles.svg}
-      width={500}
-      height={500}
-      onClick={handleClick}
-    >
-      {cirlces.map(({ x, y }, i) =>
-        <circle cx={x} cy={y} r={10} key={i}/>)}
-      {rects.map(({ x, y }, i) =>
-        <rect x={x} y={y} width={20} height={20} key={i}/>)}
-    </svg>
+      {/*
+      <button onClick={() => setTool('circle')}>Circle</button>
+      <button onClick={() => setTool('rect')}>Rect</button>
+      */}
+      <svg
+        ref={elRef}
+        className={styles.svg}
+        width={INIT_WIDTH}
+        height={INIT_HEIGHT}
+        onClick={handleClick}
+        onWheel={handleWheel}
+        viewBox="0 0 500 500"
+      >
+        {cirlces.map(({ x, y }, i) => (
+          <circle cx={x} cy={y} r={10} key={i} />
+        ))}
+        {rects.map(({ x, y }, i) => (
+          <rect x={x} y={y} width={20} height={20} key={i} />
+        ))}
+      </svg>
     </>
   )
 }
